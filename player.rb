e965799c8a35d05cbd51aa4b7fe14eee717d85a2
Jeb Alawi <alawi.5@osu.edu>
2018-05-23 15:31:00 -0400

    Player class v2

--- player.rb
+++ player.rb
@@ -2,15 +2,18 @@ require 'set'
 class Player
 
   # Author: Jeb Alawi
-  # Constructor for Player class
 
   @Name = String
   @WinningHands = Set[]
   @CurrentHand = Array.new(3)
+
+  # Constructor for Player class
   def initialize (name)
     @Name = name
   end
 
+  # returns player score
+  # score = number of winning hands
   def score
     return @WinningHands.length
   end
