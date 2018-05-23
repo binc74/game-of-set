25e5ca3fcfe88f4a275ebec745de6f527303d494
Chen Bin <chen.6133@osu.edu>
2018-05-23 16:10:11 -0400

    Implement deck.rb and write test cases for deck.rb

--- deck.rb
+++ deck.rb
@@ -1,2 +1,19 @@
+require_relative "card"
+
 class Deck
+  def initialize
+    @CardDeck = []
+    (1..3).each { |i| (1..3).each { |j| (1..3).each { |k| (1..3).each { |l| @CardDeck.push(Card.new i, j, k, l)}}}}
+  end
+
+  def length
+    @CardDeck.count
+  end
+
+  def remove
+    to_remove = Random.rand(@CardDeck.count)
+    temp = @CardDeck[to_remove]
+    @CardDeck.delete_at to_remove
+    temp
+  end
 end
