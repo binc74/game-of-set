5aff5a275c1a8666088aa0a613ec75a3ad0b5e47
Josh Wright <wright.1400@buckeyemail.osu.edu>
2018-05-23 15:02:20 -0400

    Completed Card Class

--- card.rb
+++ card.rb
@@ -1,5 +1,21 @@
 class Card
+  #Constructor method for Card class
+  # color must be {(1)red,(2)green, or (3)purple} - pick one
+  # symbol must be {(1)diamond, (2)squiggle, or (3)oval} - pick one
+  # shading must be {(1)open,(2)dashed, or (3)solid} - pick one
+  # number must be {1,2,or 3} - pick one
+  @@Colors = ["red", "green","purple"]
+  @@Symbols = ["diamond", "squiggle","oval"]
+  @@Shadings = ["open", "dashed","solid"]
+  @@Numbers = ["1", "squiggle" ,"oval"]
   def initialize (color, symbol, shading, number)
+    @color = color
+    @symbol = symbol
+    @shading = shading
+    @number = number
+  end
 
+  def toString ()
+    return  "[" + @@Colors[@color-1] + ", " + @@Symbols[@symbol-1] + ", " + @@Shadings[@shading-1] + ", " + @@Numbers[@number -1] + "]"
   end
 end
