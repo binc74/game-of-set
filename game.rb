7123661eb644b209a1f439bed8e52d0527abb6f9
Josh Wright <wright.1400@buckeyemail.osu.edu>
2018-05-23 16:48:51 -0400

    Started Game Class

--- game.rb
+++ game.rb
@@ -1,2 +1,23 @@
+require_relative "deck"
+require_relative "card"
+require_relative "player"
+
 class Game
+
+  def initialize(listOfPlayers)
+    @listOfPlayers = listOfPlayers
+    @deck = Deck.new
+    @dealersHand = []
+    unless @dealersHand.length == 12
+      if(@deck.length> 0)
+        dealersHand.push(@deck.remove)
+      end
+    end
+  end
+  #@requires hand is a list of 3 cards
+  #
+  def isSet(hand)
+
+  end
 end
+
