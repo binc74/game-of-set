e5acbfe0303501dea5cbb8b0d0f7813643a351c4
Jeb Alawi <alawi.5@osu.edu>
2018-05-23 15:29:48 -0400

    Player class

--- player.rb
+++ player.rb
@@ -1,2 +1,19 @@
+require 'set'
 class Player
+
+  # Author: Jeb Alawi
+  # Constructor for Player class
+
+  @Name = String
+  @WinningHands = Set[]
+  @CurrentHand = Array.new(3)
+  def initialize (name)
+    @Name = name
+  end
+
+  def score
+    return @WinningHands.length
+  end
+
+
 end
