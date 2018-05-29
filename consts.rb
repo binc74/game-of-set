3d652e9ec09b50f317512311795db576e0ece856
Chen Bin <chen.6133@osu.edu>
2018-05-29 14:01:44 -0400

    Add the consts.rb class

--- consts.rb
+++ consts.rb
@@ -1,2 +1,25 @@
-class Consts
+# Created by Bin Chen in 5/29
+# Bin Chen added the const for player graphics and card graphics in 5/29
+
+# this module stores all the constants that needed for this project
+module Consts
+    # Constants for player graphics
+    PLAYER_AREA_X_START = 15
+    PLAYER_AREA_X_INDENT = 15
+    PLAYER_AREA_SIZE_X = 135
+    PLAYER_AREA_SIZE_Y = 70
+    PLAYER_AREA_START_Y = 490
+    PLAYER_TEXT_INDENT_X = 10
+    PLAYER_TEXT_INDENT_Y = 15
+
+# Constants for card graphics
+    CARD_EACH_ROW = 4
+    CARD_START_X = 50
+    CARD_START_Y = 70
+    CARD_INDENT_X = 20
+    CARD_INDENT_Y = 30
+    CARD_SIZE_X = 100
+    CARD_SIZE_Y = 110
+    CARD_TEXT_INDENT_X = 30
+    CARD_TEXT_INDENT_Y = 20
 end
