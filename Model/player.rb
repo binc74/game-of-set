3b4f9d4f1eb80c779e8a559de0282b04cd70cacd
Chen Bin <chen.6133@osu.edu>
2018-05-29 14:15:46 -0400

    in player.rb, change the constructor and adding getter and setter methods

--- Model/player.rb
+++ Model/player.rb
@@ -1,30 +1,28 @@
 # Created by Jeb Alawi 5/23/18
 # Implemented by Jeb Alawi 5/23/18 - All Functionality and nearly all comments
 # Edited by Houyi Fan 5/26/18 - Complete comments and delete unused variable
+# Edited by Bin Chen 5/29/18 - Change the constructor and adding getter and setter methods
 require 'set'
 
 # Player class contains the information about the player
 class Player
 
-  # Author: Jeb Alawi
-
+  attr_reader :name, :number, :area
   #Constructor for Player class
   # name is the name of the player
   # WinningsHand contains the cards that have been identified as a part of set by the player successfully. And the number of cards in it will be the score of this player.
   def initialize (name)
-    @Name = name
-    @WinningHands = Set[]
+    @name = name
+    @number = number
+    @area = Area.new PLAYER_AREA_X_START + number * (PLAYER_AREA_X_INDENT + PLAYER_AREA_SIZE_X), PLAYER_AREA_START_Y,
+                     PLAYER_AREA_SIZE_X, PLAYER_AREA_SIZE_Y
+    @winningHands = Set[]
   end
 
   # returns player score
   # score = number of winning hands
   def score
     @WinningHands.length
-  end
-
-  # returns player name
-  def name
-    @Name
   end
 
   # requires hand is a set of 3 Card objects
