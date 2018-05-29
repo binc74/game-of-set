9eae37b36537c0fc9aac0566a55236914bc71e14
Chen Bin <chen.6133@osu.edu>
2018-05-29 13:31:59 -0400

    in card.rb, change all of the static variables into the class instance variables, add four getter method

--- Model/card.rb
+++ Model/card.rb
@@ -1,14 +1,19 @@
 # Created by Josh Wright 5/23/18
 # Implemented by Josh Wright 5/23/18 - All Functionality and comments for constructor
 # Edited by Houyi Fan 5/26/18 - Complete comments and modify coding style
+# Edited by Bin Chen 5/29/18 - change all static variables into class instance variables,adding the 4 getter
+#                                methods that get the word version of the attributes of card
 
 # Card class describes a single card which has 4 attributes (color, symbol, shading, number)
 class Card
+  # the getters and setters of this class
+  attr_reader :color, :symbol, :shading, :number, :area
+
   # class variables that describes the possible attributes a single card can have
-  @@Colors = %w{red green purple}
-  @@Symbols = %w{diamond squiggle oval}
-  @@Shadings = %w{open dashed solid}
-  @@Numbers = %w{1 2 3}
+  @Colors = %w{red green purple}
+  @Symbols = %w{diamond squiggle oval}
+  @Shadings = %w{open dashed solid}
+  @Numbers = %w{1 2 3}
 
   #Constructor method for Card class
   # color must be {(1)red,(2)green, or (3)purple} - pick one
@@ -22,29 +27,15 @@ class Card
     @number = number
   end
 
-  # returns the color of the card
-  def color
-    return @color
-  end
-
-  # returns the symbol of the card
-  def symbol
-    return @symbol
-  end
-
-  # returns the shading of the card
-  def shading
-    return @shading
-  end
-
-  # returns the number of the card
-  def number
-    return @number
-  end
+  # getters that returns the word version of the attributes of card
+  def colorStr; @@Colors[@color-1]; end
+  def symbolStr; @@Symbols[@symbol-1]; end
+  def shadingStr; @@Shadings[@shading-1] ; end
+  def numberStr; @@Numbers[@number -1]; end
 
   # returns the string representation of the card
   def toString
-    return  "[" + @@Colors[@color-1] + "," + @@Symbols[@symbol-1] + "," + @@Shadings[@shading-1] + "," + @@Numbers[@number -1] + "]"
+    return  "[" + @Colors[@color-1] + "," + @Symbols[@symbol-1] + "," + @Shadings[@shading-1] + "," + @Numbers[@number -1] + "]"
   end
 
 end
