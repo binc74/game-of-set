a9c20d4deb0f0680b5a0fa0b567b3e316eb4cade
Houyi Fan <fan.685@osu.edu>
2018-05-29 23:56:23 -0400

    Add the code that transfers each card to the corresponding image

--- view.rb
+++ view.rb
@@ -1,5 +1,6 @@
 # Created by Bin Chen 5/29/18
 # Edited by Bin Chen in 5/29/18 - implement the initialize, draw_players, draw_cards and draw methods
+# Edited by Houyi Fan in 5/29/18 - add the code that transfers each card to the corresponding image
 
 require 'gosu'
 
@@ -26,7 +27,7 @@ class View
             text_color = player.number == @game.currentPlayer ? Gosu::Color::RED : Gosu::Color::YELLOW
 
             # Draw the outer rectangle
-            #Gosu.draw_rect player.area.x, player.area.y, player.area.width, player.area.height, Gosu::Color::GREEN
+            Gosu.draw_rect player.area.x, player.area.y, player.area.width, player.area.height, Gosu::Color::GREEN
 
             # Write the Player Number
             @font.draw "#{player.name} (##{player.number + 1})", player.area.x + PLAYER_TEXT_INDENT_X,
@@ -45,21 +46,14 @@ class View
             text_color = @game.cardChosen === i ? Gosu::Color::RED : Gosu::Color::BLACK
             card = @game.dealersHand[i]
 
+            # Draw a white rectangular to represent the card area
             Gosu.draw_rect card.area.x, card.area.y, card.area.width, card.area.height, Gosu::Color::WHITE
-            # Write the Color of the card
-            @font.draw "#{card.colorStr}", card.area.x + CARD_TEXT_INDENT_X, card.area.y + 1 * CARD_TEXT_INDENT_Y, 0, 1.0, 1.0, text_color
 
-            # Write the Symbol of the card
-            @font.draw "#{card.symbolStr}", card.area.x + CARD_TEXT_INDENT_X,
-                       card.area.y + 2 * CARD_TEXT_INDENT_Y, 0, 1.0, 1.0, text_color
+            # Add corresponding image to each card
+            image = Gosu::Image.new("Images/" + card.colorStr + "_" + card.symbolStr + "_" + card.shadingStr + "_" + card.numberStr + ".PNG")
+            color = Gosu::Color.argb(0xff_ffffff) # make the color of vertices in image become white. In other words, making the image not in shadow
+            image.draw_as_quad card.area.x, card.area.y, color, card.area.x + card.area.width, card.area.y, color, card.area.x, card.area.y + card.area.height, color, card.area.x + card.area.width, card.area.y + card.area.height, color, 0
 
-            # Write the Shading of the card
-            @font.draw "#{card.shadingStr}", card.area.x + CARD_TEXT_INDENT_X,
-                       card.area.y + 3 * CARD_TEXT_INDENT_Y, 0, 1.0, 1.0, text_color
-
-            # Write the Number of the card
-            @font.draw "#{card.numberStr}", card.area.x + CARD_TEXT_INDENT_X,
-                       card.area.y + 4 * CARD_TEXT_INDENT_Y, 0, 1.0, 1.0, text_color
         }
     end
 
