c6eea9d8af26dcb94d92c44d274f7206307befd4
Chen Bin <chen.6133@osu.edu>
2018-06-01 04:00:50 -0400

    adding two additional test cases

--- testing/controller_test.rb
+++ testing/controller_test.rb
@@ -1,5 +1,6 @@
 # Created by Bin Chen 6/1/18
 # Modified by Bin Chen in 6/1/18 - adding two test cases for the is_in_area? method
+# Modified by Bin Chen in 6/1/18 - adding another two test cases for the is_in_area? method
 
 require 'test/unit'
 
@@ -15,14 +16,27 @@ class MyTest < Test::Unit::TestCase
   end
 
   # test if the location (x, y) is out of area
-  def test_is_in_area_false
+  def test_is_in_area_x_false_y_false
     area = Area.new(20, 20, 10,10)
     assert_equal false, @controller.is_in_area?(35, 35, area)
   end
 
   # test if the location (x, y) is in area
-  def test_is_in_area_true
+  def test_is_in_area_x_true_y_true
     area = Area.new(20, 20, 10,10)
     assert_equal true, @controller.is_in_area?(25, 25, area)
   end
+
+  # test if the location (x, y) is in area
+  def test_is_in_area_x_true_y_false
+    area = Area.new(20, 20, 10,10)
+    assert_equal false, @controller.is_in_area?(25, 10, area)
+  end
+
+  # test if the location (x, y) is in area
+  def test_is_in_area_x_false_y_true
+    area = Area.new(20, 20, 10,10)
+    assert_equal false, @controller.is_in_area?(10, 25, area)
+  end
+
 end
